083b483cc4b1e6ebbddaad98d6b066d744db4048
Tharindi2004 <tharindi.20240581@iit.ac.lk>
2026-03-25 22:26:09 +0530

    Dashboard

---
+++ frontend/src/pages/dashboard.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import API from "../services/api";
+import TaskForm from "../components/TaskForm";
+
+export default function Dashboard() {
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async () => {
+    const res = await API.get("/tasks");
+    setTasks(res.data);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <TaskForm refresh={fetchTasks} />
+
+      {tasks.map((t) => (
+        <div key={t._id}>
+          <h3>{t.title}</h3>
+          <p>Priority: {t.priorityScore}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
