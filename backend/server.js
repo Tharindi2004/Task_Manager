f42d2fbd83afc73d67308354d6a13ed3ccf5d630
Tharindi Thanthrige <tharamt6@gmail.com>
2026-03-25 22:10:59 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// DB connection
+connectDB();
+
+// routes
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/tasks", require("./routes/taskRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("API running...");
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
